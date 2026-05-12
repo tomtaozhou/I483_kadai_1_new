4c97a41d5af7d282ac0da2a83e01c5191d21d751
Tao Zhou <140187004+tomtaozhou@users.noreply.github.com>
2026-05-13 08:57:39 +0900

    Update rpr0521rs.py

--- 2.1_All/rpr0521rs.py
+++ 2.1_All/rpr0521rs.py
@@ -4,35 +4,48 @@ import time
 class RPR0521RS:
     ADDR = 0x38
 
-    REG_MODE_CONTROL   = 0x41
+    REG_MODE_CONTROL = 0x41
     REG_ALS_PS_CONTROL = 0x42
-    REG_PS_DATA_LSB    = 0x44
-    REG_ALS_DATA0_LSB  = 0x46
-    REG_MANUFACT_ID    = 0x92
+    REG_PS_DATA_LSB = 0x44
+    REG_ALS_DATA0_LSB = 0x46
+    REG_MANUFACT_ID = 0x92
 
     def __init__(self, i2c):
         self.i2c = i2c
         self.i2c.readfrom_mem(self.ADDR, self.REG_MANUFACT_ID, 1)
-        self.i2c.writeto_mem(self.ADDR, self.REG_MODE_CONTROL, bytes([0xC6]))
-        self.i2c.writeto_mem(self.ADDR, self.REG_ALS_PS_CONTROL, bytes([0x02]))
+
+        self.i2c.writeto_mem(self.ADDR, self.REG_MODE_CONTROL, bytes([0x00]))
+        time.sleep_ms(50)
+
+        self.i2c.writeto_mem(self.ADDR, self.REG_MODE_CONTROL, bytes([0x89, 0x02]))
+
         self.gain0 = 1
         self.gain1 = 1
         self.meas_time = 100
-        time.sleep_ms(200)
+
+        time.sleep_ms(500)
 
     def read_ps(self):
         data = self.i2c.readfrom_mem(self.ADDR, self.REG_PS_DATA_LSB, 2)
         return (data[0] | (data[1] << 8)) & 0x0FFF
 
-    def read_lux(self):
+    def read_raw_als(self):
         data = self.i2c.readfrom_mem(self.ADDR, self.REG_ALS_DATA0_LSB, 4)
         data0 = data[0] | (data[1] << 8)
         data1 = data[2] | (data[3] << 8)
+        return data0, data1
+
+    def read_lux(self):
+        data0, data1 = self.read_raw_als()
+
         if data0 == 0:
             return 0.0
+
         d0 = data0 / self.gain0 * (100.0 / self.meas_time)
         d1 = data1 / self.gain1 * (100.0 / self.meas_time)
+
         ratio = d1 / d0
+
         if ratio < 0.595:
             lux = 1.682 * d0 - 1.877 * d1
         elif ratio < 1.015:
@@ -43,4 +56,5 @@ class RPR0521RS:
             lux = 0.766 * d0 - 0.250 * d1
         else:
             lux = 0.0
+
         return max(0.0, lux)
